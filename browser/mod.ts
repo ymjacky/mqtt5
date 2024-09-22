d5cbafafbfdbce35601c8a8fa12063a56315c30b
ymjacky <jacky.service@icloud.com>
2024-09-23 01:50:54 +0900

    fix lint error

--- browser/mod.ts
+++ browser/mod.ts
@@ -1,7 +1,6 @@
 export * as Mqtt from '../lib/mqtt/mod.ts';
 export * as MqttPackets from '../lib/mqtt_packets/mod.ts';
 export * as MqttProperties from '../lib/mqtt_properties/mod.ts';
-export * as MqttUtils from '../lib/mqtt_utils/mod.ts';
 
 export * as ClientTypes from '../lib/client/client_types.ts';
 export { WebSocketMqttClient } from '../lib/client/ws_mqtt_client.ts';
--- deno/mod.ts
+++ deno/mod.ts
@@ -279,6 +279,5 @@
 export * as Mqtt from '../lib/mqtt/mod.ts';
 export * as MqttPackets from '../lib/mqtt_packets/mod.ts';
 export * as MqttProperties from '../lib/mqtt_properties/mod.ts';
-export * as MqttUtils from '../lib/mqtt_utils/mod.ts';
 export * from '../lib/client/mod.ts';
 export * as Cache from '../lib/cache/mod.ts';
